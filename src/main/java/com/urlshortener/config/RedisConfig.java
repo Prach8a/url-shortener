1bf759d2b5529c40d36c0694e1c2d02b4ba8a770
Pracheta Patil <prachetavpatil@gmail.com>
2026-07-15 23:10:40 +0530

    MAKING REDISSS WORKKKK

--- src/main/java/com/urlshortener/config/RedisConfig.java
+++ src/main/java/com/urlshortener/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.urlshortener.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,12 +11,13 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.data.redis.host")  // <- ADD THIS
 public class RedisConfig {
     
-    @Value("${spring.data.redis.host}")
+    @Value("${spring.data.redis.host:localhost}")
     private String redisHost;
     
-    @Value("${spring.data.redis.port}")
+    @Value("${spring.data.redis.port:6379}")
     private int redisPort;
     
     @Bean
